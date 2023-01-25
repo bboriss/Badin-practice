e27fcfa9206a9b11e0c9a374864104d283310d73
Boris Stevanovic <boriss@boris.stevanovic>
2023-01-25 15:18:22 +0100

    client animation updated

--- Badin Site/scripts/app.js
+++ Badin Site/scripts/app.js
@@ -4,6 +4,35 @@ import { moveToLeft, moveToRight, getBackPosition } from "./trustedBy.js";
 import { moveToRightGallery, moveToLeftGallery } from "./badinGallery.js";
 import { galleryIntersecting } from "./galleryObserver.js";
 
+const logosRef = Array.from(
+  document.getElementsByClassName("client_icon")
+).splice(0, 13);
+const logoContainer = document.querySelector(
+  ".trusted_by_second-icons_container_content"
+);
+
+let logos = [...logosRef];
+let i = 0;
+
+const logoChanger = () => {
+  while (logoContainer.firstElementChild) {
+    logoContainer.firstElementChild.remove();
+  }
+
+  logos.forEach((logo) => logo.classList.remove("first_iteration"));
+
+  let firstFive = logos.splice(0, 5);
+  console.log("first five: ", firstFive);
+
+  logos = [...logos, ...firstFive];
+
+  logos.slice(0, 5).forEach((logo) => logoContainer.append(logo));
+};
+
+if (window.innerWidth > 768) {
+  setInterval(logoChanger, 3000);
+}
+
 // trusted by section arrows
 const arrLeft = document.querySelector(".arrow-left");
 const arrRight = document.querySelector(".arrow-right");
@@ -32,6 +61,7 @@ arrLeft.addEventListener("click", moveToLeft);
 arrRight.addEventListener("click", moveToRight);
 // when window resized get back picture container to position 0
 window.addEventListener("resize", getBackPosition);
+// window.addEventListener("resize", clientAnimationTimer);
 
 // <--- back to top btn --->
 window.addEventListener("scroll", buttonFiller);
