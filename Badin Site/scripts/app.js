e857d2d1c0eca4b1641cc8be4b8d4bf8a9d4d7d5
Boris Stevanovic <boriss@boris.stevanovic>
2023-01-26 09:25:35 +0100

    thursday morning

--- Badin Site/scripts/app.js
+++ Badin Site/scripts/app.js
@@ -5,8 +5,9 @@ import { moveToRightGallery, moveToLeftGallery } from "./badinGallery.js";
 import { galleryIntersecting } from "./galleryObserver.js";
 
 const logosRef = Array.from(
-  document.getElementsByClassName("client_icon")
+  document.getElementsByClassName("client_icondesktop")
 ).splice(0, 13);
+
 const logoContainer = document.querySelector(
   ".trusted_by_second-icons_container_content"
 );
@@ -14,18 +15,21 @@ const logoContainer = document.querySelector(
 let logos = [...logosRef];
 
 const logoChanger = () => {
-  while (logoContainer.firstElementChild) {
-    logoContainer.firstElementChild.remove();
-  }
-
+  logoContainer.innerHTML = "";
+  // remove hardcoded visible logos
   logos.forEach((logo) => logo.classList.remove("first_iteration"));
-
+  // move them to the end of array
   let firstFive = logos.splice(0, 5);
-  console.log("first five: ", firstFive);
 
   logos = [...logos, ...firstFive];
+  // take second 5 to display
+  let forDisplay = logos.slice(0, 5);
 
-  logos.slice(0, 5).forEach((logo) => logoContainer.append(logo));
+  for (let i = 0; i < forDisplay.length; i++) {
+    setTimeout(() => {
+      logoContainer.append(forDisplay[i]);
+    }, 150 * i);
+  }
 };
 
 const test = setInterval(logoChanger, 3000);
@@ -58,19 +62,6 @@ arrLeft.addEventListener("click", moveToLeft);
 arrRight.addEventListener("click", moveToRight);
 // when window resized get back picture container to position 0
 window.addEventListener("resize", getBackPosition);
-// window.addEventListener("resize", () => {
-//   if (window.innerWidth < 768) {
-//     clearInterval(test);
-//     console.log("smanjeno");
-//   }
-// });
-// window.addEventListener("DOMContentLoaded", () => {
-//   if (window.innerWidth < 768) {
-//     clearInterval(test);
-//   } else {
-//     setInterval(test);
-//   }
-// });
 
 // <--- back to top btn --->
 window.addEventListener("scroll", buttonFiller);
